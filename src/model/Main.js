4fef78b2a3aed193b5171b45b3bd40916a65b576
Allie <allielinhdo@gmail.com>
2018-04-21 13:37:19 -0400

    Instructions modified

--- src/model/Main.js
+++ src/model/Main.js
@@ -10,7 +10,7 @@ const Main = () => (
   <main>
       <div className="container">
       <section className="play">
-          <h1 className="section-heading mt-5 pt-4 font-weight-bold wow fadeIn">About MDB</h1>
+          <h1 className="section-heading mt-5 pt-4 font-weight-bold wow fadeIn">About Vocs</h1>
           <div className="row wow fadeIn">
               <div className="col-lg-6 col-md-12 px-4">
                   <div className="row">
@@ -18,9 +18,8 @@ const Main = () => (
                           <i className="fa fa-code fa-2x indigo-text"></i>
                       </div>
                       <div className="col-10">
-                          <h5 className="feature-title">Bootstrap 4</h5>
-                          <p className="grey-text">Thanks to MDB you can take advantage of all feature of newest
-                              Bootstrap 4.</p>
+                          <h5 className="feature-title">Vocs</h5>
+                          <p className="grey-text">Thanks to VOCS, you are now introduced to a new world of easier communication.</p>
                       </div>
                   </div>
                   <div style={styles.block}></div>
@@ -29,10 +28,8 @@ const Main = () => (
                           <i className="fa fa-book fa-2x blue-text"></i>
                       </div>
                       <div className="col-10">
-                          <h5 className="feature-title">Detailed documentation</h5>
-                          <p className="grey-text">We give you detailed user-friendly documentation at your disposal. It
-                              will help you to implement your ideas
-                              easily.
+                          <h5 className="feature-title">How To Use VOCS</h5>
+                          <p className="grey-text">Simply press start and start recording. Your speech will be automatically transcribed into text and analyzed using Natural Languange Processing.
                           </p>
                       </div>
                   </div>
@@ -42,17 +39,17 @@ const Main = () => (
                           <i className="fa fa-graduation-cap fa-2x cyan-text"></i>
                       </div>
                       <div className="col-10">
-                          <h5 className="feature-title">Lots of tutorials</h5>
-                          <p className="grey-text">We care about the development of our users. We have prepared numerous
-                              tutorials, which allow you to learn
-                              how to use MDB as well as other technologies.</p>
+                          <h5 className="feature-title">Special Features</h5>
+                          <p className="grey-text">We inspect your speech for sentiments and entities. We provide a pie chart with emotions and entities together with their confidence levels. Entities is a new concept for reference to the unique name
+of a real-world object such as people, places, and commercial items, and to
+precise references to measures such as dates and quantities</p>
                       </div>
                   </div>
               </div>
               <div className="col-lg-6 col-md-12">
                   <p className="h5 text-center mb-4">MKBHD</p>
                   <div className="embed-responsive embed-responsive-16by9">
-                      <iframe src="https://www.youtube.com/embed/4O9dNuiOWDg" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title="video"></iframe>
+                      <iframe src="CI.png" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title="video"></iframe>
                   </div>
               </div>
           </div>
